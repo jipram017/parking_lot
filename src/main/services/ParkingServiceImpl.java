1e0ee129151c1305da9b28e07cf871402641bf53
jipram019 <jipram019@gmail.com>
2020-10-26 19:14:39 +0700

    added implementation of parking service

---
+++ src/main/services/ParkingServiceImpl.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package main.services;
+
+import java.util.Optional;
+
+import main.models.Vehicle;
+
+/**
+ * @author ajipramono
+ *
+ */
+public class ParkingServiceImpl implements ParkingService {
+
+	@Override
+	public void createParkingLot(int capacity) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Optional<Integer> park(Vehicle vehicle) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void unPark(int slot) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Optional<Integer> getAvailableSlot() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void getStatus() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getRegNumFromColor(String color) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getSlotNumFromRegNo(String regNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getSlotNumFromColor(String color) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
