b7928338fb38e6a5042247f07586ee9e4289f4cd
jipram019 <jipram019@gmail.com>
2020-10-26 19:17:13 +0700

    added parking manager implementation

--- src/main/dao/ParkingManager.java
+++ src/main/dao/ParkingManager.java
@@ -13,8 +13,8 @@ import main.models.Vehicle;
  */
 public interface ParkingManager<T extends Vehicle> {
 	public int parkCar(T Vehicle);
-	public boolean unParkCar(T Vehicle, String slot);
-	public List<String> getStatus();
+	public boolean unParkCar(int slot);
+	public List<String> getParkingStatus();
 	public List<String> getRegNumsFromColor(String color);
 	public List<Integer> getSlotNumsFromColor(String color);
 	public int getSlotNumFromRegNum(String regNum);
--- src/main/dao/ParkingManagerImpl.java
+++ src/main/dao/ParkingManagerImpl.java
@@ -3,7 +3,11 @@
  */
 package main.dao;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import main.models.Allocation;
@@ -18,6 +22,9 @@ public class ParkingManagerImpl<T extends Vehicle> implements ParkingManager<T> 
 	private AtomicInteger capacity = new AtomicInteger();
 	private AtomicInteger availability = new AtomicInteger();
 	private Allocation allocation;
+	
+	// to map from a slot to a Vehicle
+	private Map<Integer, Optional<T>> slotVehicleMap;
 	
 	@SuppressWarnings("rawtypes")
 	private static ParkingManagerImpl instance = null;
@@ -45,7 +52,9 @@ public class ParkingManagerImpl<T extends Vehicle> implements ParkingManager<T> 
 		if(allocation == null) {
 			allocation = new AllocationImpl();
 		}
-		for(int i=0; i<capacity; i++) {
+		this.slotVehicleMap = new ConcurrentHashMap<>();
+		for(int i=1; i<capacity; i++) {
+			slotVehicleMap.put(i, Optional.empty());
 			allocation.addSlot(i);
 		}
 	}
@@ -55,45 +64,85 @@ public class ParkingManagerImpl<T extends Vehicle> implements ParkingManager<T> 
 	}
 	
 	@Override
-	public int parkCar(T Vehicle) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int parkCar(T vehicle) {
+		int availableSlot;
+		if(availability.get() == 0) {
+			return 0;
+		}
+		else {
+			availableSlot = allocation.getSlot();
+			if(slotVehicleMap.containsValue(Optional.of(vehicle))){
+				return -1;
+			}
+			slotVehicleMap.put(availableSlot,  Optional.of(vehicle));
+			availability.decrementAndGet();
+			allocation.removeSlot(availableSlot);
+		}
+		return availableSlot;
 	}
 
 	@Override
-	public boolean unParkCar(T Vehicle, String slot) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean unParkCar(int slot) {
+		if(!slotVehicleMap.get(slot).isPresent()) {
+			return false;
+		}
+		availability.getAndIncrement();
+		allocation.addSlot(slot);
+		slotVehicleMap.put(slot, Optional.empty());
+		return true;
 	}
 
 	@Override
-	public List<String> getStatus() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<String> getParkingStatus() {
+		List<String> statusList = new ArrayList<String>();
+		for(int i=1; i<=capacity.get(); i++) {
+			Optional<T> vehicle = slotVehicleMap.get(i);
+			if(vehicle.isPresent()) {
+				statusList.add(i + "\t\t" + vehicle.get().getRegNum() + "\t\t" + vehicle.get().getColor());
+			}
+		}
+		return statusList;
 	}
 
 	@Override
 	public List<String> getRegNumsFromColor(String color) {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> regNums = new ArrayList<String>();
+		for(int i=1; i <= capacity.get(); i++) {
+			Optional<T> vehicle = slotVehicleMap.get(i);
+			if(vehicle.isPresent() && color.equalsIgnoreCase(vehicle.get().getColor())){
+				regNums.add(vehicle.get().getRegNum());
+			}
+		}
+		return regNums;
 	}
 
 	@Override
 	public List<Integer> getSlotNumsFromColor(String color) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Integer> slotNums = new ArrayList<Integer>();
+		for(int i=1; i<= capacity.get(); i++) {
+			Optional<T> vehicle = slotVehicleMap.get(i);
+			if(vehicle.isPresent() && color.equalsIgnoreCase(vehicle.get().getColor())){
+				slotNums.add(i);
+			}
+		}
+		return slotNums;
 	}
 
 	@Override
 	public int getSlotNumFromRegNum(String regNum) {
-		// TODO Auto-generated method stub
-		return 0;
+		int slotNum = -1;
+		for(int i=1; i<= capacity.get(); i++) {
+			Optional<T> vehicle = slotVehicleMap.get(i);
+			if(vehicle.isPresent() && regNum.equalsIgnoreCase(vehicle.get().getRegNum())){
+				slotNum = i;
+			}
+		}
+		return slotNum;
 	}
 
 	@Override
 	public int getAvailableSlot() {
-		// TODO Auto-generated method stub
-		return 0;
+		return availability.get();
 	}
 
 }
